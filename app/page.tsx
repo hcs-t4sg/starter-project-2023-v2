007385ddeb541d0a4ec67ce178ed68383c425cba
IsoPhoenix <IsoPhoenix@users.noreply.github.com>
2023-06-08 05:41:31 +0000

    Fix formatting and auto-fixable linting errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
 import { TypographyH2, TypographyP } from "@/components/ui/typography";
 
 export default function Home() {
-
-
-
-
-
-
-
-
-  
-
-
   return (
     <main>
       <TypographyH2>Welcome to the T4SG starter project!</TypographyH2>
