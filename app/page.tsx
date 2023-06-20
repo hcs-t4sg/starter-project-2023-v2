8015b45ae9ba07335a6a971f4ae154141911895e
Matthew Su <matthewsu@college.harvard.edu>
2023-06-20 12:27:59 -0400

    tesst

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main>
       <TypographyH2>Welcome to the T4SG starter project!</TypographyH2>
       <TypographyP>
-        This starter project is styled with Tailwind CSS and uses shadcn/ui as a component library. Feel free to add
+        This starter project is styled with Tailwind CSS and uses shadcn/ui as a component library. Feel free to addsdf
         your own components!
       </TypographyP>
     </main>
